8177c38e1284b9c5f44c27ac1af2d15fb6d7462e
yakateryna <124745292+yakateryna@users.noreply.github.com>
2023-07-25 16:55:42 +0200

    task 2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,26 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+// Отримуємо посилання на елемент <ul> за його id
+const ul = document.getElementById("ingredients");
+
+// Створюємо фрагмент для швидшої операції вставки
+const fragment = document.createDocumentFragment();
+
+// Проходимося по кожному елементу масиву ingredients
+ingredients.forEach((ingredient) => {
+  // Створюємо новий елемент <li>
+  const li = document.createElement("li");
+  
+  // Додаємо текстовий вміст з назвою інгредієнта
+  li.textContent = ingredient;
+  
+  // Додаємо клас "item"
+  li.classList.add("item");
+  
+  // Додаємо елемент <li> до фрагмента
+  fragment.appendChild(li);
+});
+
+// Вставляємо фрагмент зі всіма елементами <li> у <ul> за одну операцію
+ul.appendChild(fragment);
